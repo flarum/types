5fa477842d6f209510dab4b1c085e443a49f0f76
David Sevilla Martin <me@datitisev.me>
2021-02-28 15:52:55 -0500

    Initial commit

---
+++ generate.sh
@@ -0,0 +1,54 @@
+set -e
+
+rm -rf types/
+rm -rf repos/
+
+REMOTE=`git rev-parse --abbrev-ref --symbolic-full-name @{u}`
+REMOTE_PARSED=(${REMOTE//\// })
+git clone `git remote get-url ${REMOTE_PARSED[0]}` types
+
+# generate <ref> <npm version>
+generate() {
+    REF=$1
+    VERSION=$2
+
+    rm -rf types/*
+
+    # clone repository
+    mkdir -p repos/core
+    git clone https://github.com/flarum/core.git repos/core
+    (cd repos/core && git checkout $REF)
+
+    # install dependencies
+    (cd repos/core/js && npm ci)
+
+    cd types/
+
+    # create package.json
+    cat > package.json << EOF
+{
+    "name": "flarum",
+    "version: "$VERSION"
+}
+EOF
+
+
+    # generate typings
+    set +e
+    tsc -p ../tsconfig.json
+    set -e
+
+    cd ..
+}
+
+push() {
+    # push typings to repository
+    cd types
+    git add .
+    git commit -m "Generate Flarum $1 typings" --author "GitHub Action <action@github.com>"
+    git push 
+}
+
+
+generate master 0.1.0-beta.16-dev
+push 0.1.0-beta.16
